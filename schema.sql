7a82c49ce078dd8604a9eb63ddb31696684005bd
Amar Shah <jayshahindia@yahoo.com>
2022-02-16 23:20:36 -0800

    Initial Commit

--- schema.sql
+++ schema.sql
@@ -31,13 +31,14 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+DROP TABLE titles;
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title varchar NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title)
 );
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
